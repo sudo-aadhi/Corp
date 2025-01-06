f801be18f50cf815fcfebe4d201550f4f3d2b896
sudo-aadhi <sudoaadhi@gmail.com>
2025-01-06 20:54:03 +0530

    Header component created

---
+++ app/config/header/config.js
@@ -0,0 +1,18 @@
+export const header = [
+  {
+    title: "About",
+    key: "about",
+  },
+  {
+    title: "Products",
+    key: "products",
+  },
+  {
+    title: "Account",
+    key: "account",
+  },
+  {
+    title: "Contact",
+    key: "contact",
+  },
+];
